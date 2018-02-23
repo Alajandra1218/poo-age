b23aa3851c9b2b3bdd975ce06317ca894632ce4d
T-102 <T-102@PC249>
2018-02-22 19:40:18 -0600

    exepciones

---
+++ poo-age/src/Exepciones1/Primera.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exepciones1;
+
+/**
+ *
+ * @author T-102
+ */
+public class Primera {
+    public static void main(String[] args) {
+        //vamos a crear un arreglo
+        int[] valores={2,6,-8,5};
+        for(int i=0;i<=valores.length;i++)
+        {
+           // System.out.println("Valor "+valores[i]);
+       
+        }
+       // esta exepción es muy truculenta
+       float x=5;
+       int y=0;
+        //System.out.println(x/y); ArithmeticException
+         int z=Integer.parseInt("x");
+         System.out.println(z*2);//numberformatexception
+    
+    
+    
+    }
+    
+    
+    
+    
+}
